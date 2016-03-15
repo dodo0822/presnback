0a44dbe7cfda96604d9363bdd3fd1ab6a53baa2e
Chih-Wei Fang <dodo0822@hs.ntnu.edu.tw>
2016-03-15 11:25:31 +0800

    remove config.js from revision control

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	siteUrl: 'http://localhost:8081',
-	mailgunKey: 'key-afb290b7c1348a5860483a3d1b8d435b',
-	mailgunDomain: 'sandbox70a2aaf68b554d2e95596503d36404d5.mailgun.org'
-};
